21822a538e231cd889d770daee7df885519f7d09
Saw Sandy <sawsandy510.sgg@gmail.com>
2025-09-09 17:23:52 +0630

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+
+<script>
+    document.getElementById("sub-btn").addEventListener("click", function () {
+        window.location.href = "http://127.0.0.1:5501/creditcard.html"
+    });
+</script>
+
+
+
